18afd2db609298bf7fa6e1e6f7812bdaf2541be1
Ben Yang <benayang@gmail.com>
2019-04-19 19:49:55 -0700

    removed extraneous stuff in sample html, version bump

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const URLBASE = (VERSION === 2) ? APISERVER + '/v2' : APISERVER
 function query(ep, params, get = false) {
   let url = new URL(URLBASE + ep);
   let headers = new Headers({
-    'x-app-name': `panlex-language-picker/1.0.0`,
+    'x-app-name': `panlex-language-picker/1.1.0`,
     'content-type': 'application/json',
   });
   return (fetch(url, {
